2b7f7534c2dbce5c6b5da1eea597cc3971321c38
Gabe Conradi <gabe@tumblr.com>
2015-06-22 11:41:57 -0400

    make firehose-agent take a json config

--- firehose-agent/main.go
+++ firehose-agent/main.go
@@ -9,19 +9,25 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"os"
 	"strconv"
 	"time"
 )
 
 var (
-	host      string
-	streamid  string
-	user      string
-	pass      string
-	redisHost string
-	channel   string
-	debug     bool
+	configfile string
+	config     AgentConfig
 )
+
+type AgentConfig struct {
+	Host      string `json:"host"`
+	Streamid  string `json:"stream"`
+	User      string `json:"username"`
+	Pass      string `json:"password"`
+	RedisHost string `json:"redis-host"`
+	Channel   string `json:"channel"`
+	Debug     bool   `json:"debug"`
+}
 
 type FirehoseMessage struct {
 	Id           string       `json:"id"`
@@ -51,13 +57,13 @@ type EventMessage struct {
 }
 
 func updatePosition(ts int64, client http.Client) (*http.Response, error) {
-	if debug {
+	if config.Debug {
 		log.Printf("Updating position to %d", ts)
 	}
-	checkpointurl := fmt.Sprintf("http://%s/2.2/firehose/%s/position?timestamp=%d", host, streamid, ts)
+	checkpointurl := fmt.Sprintf("http://%s/2.2/firehose/%s/position?timestamp=%d", config.Host, config.Streamid, ts)
 	req, err := http.NewRequest("PUT", checkpointurl, nil)
 	req.Header.Set("Accept-Encoding", "gzip")
-	req.SetBasicAuth(user, pass)
+	req.SetBasicAuth(config.User, config.Pass)
 	if err != nil {
 		log.Printf("Unable to create request to update position to %d: %s", ts, err)
 	}
@@ -66,34 +72,48 @@ func updatePosition(ts int64, client http.Client) (*http.Response, error) {
 		log.Printf("Unable to update position to %d: %s", ts, err)
 		return nil, err
 	}
-	if debug {
+	if config.Debug {
 		log.Printf("Updated position to %d: %s", ts, resp.Status)
 	}
 	return resp, nil
 }
 
 func init() {
-	flag.StringVar(&host, "host", "", "Firehose endpoint (i.e. host:8080)")
-	flag.StringVar(&streamid, "stream", "radiator", "Firehose stream ID (unique client id)")
-	flag.StringVar(&user, "username", "gabe", "Firehose stream username")
-	flag.StringVar(&pass, "password", "", "Firehose stream password")
-	flag.StringVar(&redisHost, "redis-host", ":6379", "redis host:port to attach to and publish to")
-	flag.StringVar(&channel, "channel", "firehose", "channel to publish events into")
-	flag.BoolVar(&debug, "debug", false, "print debugs")
+	flag.StringVar(&config.Host, "host", "", "Firehose endpoint (i.e. host:8080)")
+	flag.StringVar(&config.Streamid, "stream", "radiator", "Firehose stream ID (unique client id)")
+	flag.StringVar(&config.User, "username", "gabe", "Firehose stream username")
+	flag.StringVar(&config.Pass, "password", "", "Firehose stream password")
+	flag.StringVar(&config.RedisHost, "redis-host", ":6379", "redis host:port to attach to and publish to")
+	flag.StringVar(&config.Channel, "channel", "firehose", "channel to publish events into")
+	flag.BoolVar(&config.Debug, "debug", false, "print debugs")
+	flag.StringVar(&configfile, "config", "", "load configuration in from config json")
 	flag.Parse()
 }
 
 func main() {
+	// if config is specified, load that
+	if configfile != "" {
+		config = AgentConfig{}
+		f, err := os.Open(configfile)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer f.Close()
+		err = json.NewDecoder(f).Decode(&config)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
 	// set up a redis connection
-	rclient := redis.NewClient(&redis.Options{Addr: redisHost})
+	rclient := redis.NewClient(&redis.Options{Addr: config.RedisHost})
 	_, err := rclient.Ping().Result()
 	if err != nil {
 		log.Fatal(err)
 	}
-	log.Printf("Redis client %s active", redisHost)
+	log.Printf("Redis client %s active", config.RedisHost)
 	defer rclient.Close()
 
-	url := fmt.Sprintf("http://%s/2.2/firehose/%s/stream", host, streamid)
+	url := fmt.Sprintf("http://%s/2.2/firehose/%s/stream", config.Host, config.Streamid)
 	client := &http.Client{}
 
 	log.Printf("Skipping forward in the firehose to right now")
@@ -104,7 +124,7 @@ func main() {
 		log.Printf("Attempting connection to %s", url)
 		req, err := http.NewRequest("GET", url, nil)
 		req.Header.Set("Accept-Encoding", "gzip")
-		req.SetBasicAuth(user, pass)
+		req.SetBasicAuth(config.User, config.Pass)
 
 		/*
 			tschan := make(chan int64)
@@ -134,7 +154,7 @@ func main() {
 		}
 		decoder := json.NewDecoder(gz)
 		defer gz.Close()
-		log.Printf("Publishing posts to %s", channel)
+		log.Printf("Publishing posts to %s", config.Channel)
 		for {
 			var m FirehoseMessage
 			if err := decoder.Decode(&m); err == io.EOF {
@@ -176,10 +196,10 @@ func main() {
 				if err != nil {
 					log.Printf("%s", err)
 				}
-				rclient.Publish(channel, string(j[:]))
+				rclient.Publish(config.Channel, string(j[:]))
 				//tschan <- m.Timestamp
-				if debug {
-					log.Printf("PUBLISH %s %s", channel, w)
+				if config.Debug {
+					log.Printf("PUBLISH %s %s", config.Channel, w)
 				}
 
 			}
